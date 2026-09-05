05095a0883e0bca85c05eae65c67de7f1b03e007
Hossam Ragab <captenavira233@gmail.com>
2026-09-05 19:58:30 +0300

    add new php phases for testing

--- routes/console.php
+++ routes/console.php
@@ -1,11 +1,12 @@
 <?php
 
-Schedule::command('lab:heartbeat')->everyMinute();
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
+Schedule::command('lab:heartbeat')->everyMinute();
+
 Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
+    // $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
